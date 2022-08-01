ce9abbeaa50f04167a1ec08d91307793194ddc82
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-08-01 20:32:40 +0545

    hiding api keys

---
+++ app/src/main/java/com/chaudharynabin6/newapp/data/datasources/remote/NewsAPI.kt
@@ -0,0 +1,19 @@
+package com.chaudharynabin6.newapp.data.datasources.remote
+
+import com.chaudharynabin6.newapp.BuildConfig
+import com.chaudharynabin6.newapp.data.datasources.remote.dto.NewsResponseDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsAPI {
+
+    @GET("v2/everything?")
+    suspend fun getNews(
+        @Query(value = "apiKey") apiKey : String = BuildConfig.API_KEY,
+        @Query(value = "q") q : String
+    ) : NewsResponseDto
+
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
