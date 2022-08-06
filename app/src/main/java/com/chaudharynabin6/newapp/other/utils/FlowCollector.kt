cfbef8bbcd33895d6b9993ff3cfdce074e799669
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-08-06 11:47:42 +0545

    flow collector in fragment and activity

---
+++ app/src/main/java/com/chaudharynabin6/newapp/other/utils/FlowCollector.kt
@@ -0,0 +1,30 @@
+package com.chaudharynabin6.newapp.other.utils
+
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+fun <T> LifecycleOwner.collectLatestLiveCycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collectLatest {
+                collect(it)
+            }
+        }
+    }
+}
+
+fun <T> LifecycleOwner.collectLiveCycleFlow(flow: Flow<T>, collect: suspend (T) -> Unit) {
+    lifecycleScope.launch {
+        repeatOnLifecycle(Lifecycle.State.STARTED) {
+            flow.collect {
+                collect(it)
+            }
+        }
+    }
+}
